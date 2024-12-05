e3672b61afcf94b60545823521e2d3e196b3874d
tseaver6 <dancerfool@gmail.com>
2024-12-05 00:15:57 -0500

    day5 top one thousand baby part two im on fire

---
+++ 2024/day5/part-two.ts
@@ -0,0 +1,50 @@
+import { logResult, readFile, logBenchmark } from "../util";
+logBenchmark(performance.now());
+const inputString = readFile(process.argv[2]);
+const [ruleString, updateString] = inputString.split("\n\n");
+const ruleList = ruleString
+  .split("\n")
+  .map((rule) => rule.split("|").map((page) => parseInt(page)));
+const updateList = updateString
+  .split("\n")
+  .map((update) => update.split(",").map((page) => parseInt(page)));
+
+logResult(
+  updateList
+    .filter((update) => {
+      for (let pageIndex = 0; pageIndex < update.length; pageIndex += 1) {
+        const currentPage = update[pageIndex];
+        const previousPages = update.slice(0, pageIndex);
+        for (const [, followingPage] of ruleList.filter(
+          ([priorPage]) => priorPage === currentPage
+        )) {
+          if (previousPages.includes(followingPage)) {
+            return true;
+          }
+        }
+      }
+      return false;
+    })
+    .map((update) => {
+      for (let pageIndex = 0; pageIndex < update.length; pageIndex += 1) {
+        const currentPage = update[pageIndex];
+        const previousPages = update.slice(0, pageIndex);
+        for (const [, followingPage] of ruleList.filter(
+          ([priorPage]) => priorPage === currentPage
+        )) {
+          if (previousPages.includes(followingPage)) {
+            const improperPageIndex = previousPages.indexOf(followingPage);
+            update[improperPageIndex] = currentPage;
+            update[pageIndex] = followingPage;
+            pageIndex = 0;
+            break;
+          }
+        }
+      }
+      return update;
+    })
+    .map((update) => update[Math.floor(update.length / 2)])
+    .reduce((prev, acc) => prev + acc, 0)
+);
+
+logBenchmark(performance.now());
